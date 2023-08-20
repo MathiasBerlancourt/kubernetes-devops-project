1d1074655371c076040ae1bc9a03696dbad80212
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-08-20 16:14:23 +0200

    update db.y

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:password@localhost:3306/storedb")
+engine = create_engine(
+    "mysql+pymysql://root:password@mysql-service:3306/storedb")
 
 meta = MetaData()
 
