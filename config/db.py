a0b35967f5a0586bd3c7ffd62b1cff4373135e84
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-08-25 11:33:55 +0200

    update create engine with mysql

--- config/db.py
+++ config/db.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine, MetaData
+import base64
+import os
+
+encoded_password = os.environ.get("MYSQL_ROOT_PASSWORD")
+
+
+password = base64.b64decode(encoded_password).decode("utf-8")
+
 
 engine = create_engine(
-    "mysql+pymysql://root:password@mysql:3306/storedb")
+    "mysql+pymysql://root:{password}@mysql-service:3306/storedb")
 
 meta = MetaData()
 
